052bb24d89094fc1fd88532db333e108454b2d2f
NITHISH-KUMAR-K-7 <grknithishkumar@gmail.com>
2025-03-21 09:52:05 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/weather-report/",
+  base: "/Weather-Report/",
   plugins: [
     tailwindcss(),
     react()],
